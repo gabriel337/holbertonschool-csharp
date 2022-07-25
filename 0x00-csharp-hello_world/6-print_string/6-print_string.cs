5d8767335a69bd3706ea087630299c5bda409042
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-25 19:27:26 -0400

    add task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string repeatedString = string.Concat(Enumerable.Repeat(str,3));
+        Console.WriteLine(repeatedString);
+        string halfString = str.Substring(0,9);
+        Console.WriteLine(halfString);
+        }
+}
