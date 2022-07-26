7d41562dc48209447fb2745f53c918b31c3c41b9
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 17:16:08 -0400

    add task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        for (char lowerLetter = 'a'; lowerLetter <= 'z'; lowerLetter++)
+        {
+            if (lowerLetter == c)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
