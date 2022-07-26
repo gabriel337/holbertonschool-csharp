c002f6e720b1bf9430fc0fab716fca3308283e72
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-25 23:37:25 -0400

    add task 100

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static int Main(string[] args)
+	{
+        TextWriter errorWriter = Console.Error;
+        errorWriter.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        return (1);
+        }
+}
