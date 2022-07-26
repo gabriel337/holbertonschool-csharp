4fda4215ff44220cb7ed65a0a4a8795945c2c361
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 14:21:40 -0400

    add task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num <= 98; num++)
+            {
+                Console.Write("{0:D} = 0x{0:x}\n",num);
+            }
+
+    }
+}
