b4c9e6a33858e41ecbfc67530d6d9545481c8c41
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 14:40:22 -0400

    add task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num <= 98; num++)
+        {
+            Console.Write(string.Format("{0:00}, ", num));
+            
+        }
+        Console.Write("99\n");
+    }
+}
