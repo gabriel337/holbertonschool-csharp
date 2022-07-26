affb813d2f92549e219889800fa6e0a1d9fc46ab
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-25 20:58:02 -0400

    add task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:0.00%}\nCurrency: {1:C}", percent, currency);
+	}
+}
