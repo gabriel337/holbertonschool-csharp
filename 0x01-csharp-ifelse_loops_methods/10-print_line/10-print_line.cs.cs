87711d62900b59b8144c5bfefee1a5208f2489d9
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 17:42:08 -0400

    change typo

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            string str = new string('_', length);
+            Console.Write(str + "\n");
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
