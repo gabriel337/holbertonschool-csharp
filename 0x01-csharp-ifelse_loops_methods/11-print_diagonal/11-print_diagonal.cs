17dbe20a1334a3951ccebf4dd1d30f9811ea47cb
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 18:04:25 -0400

    add task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length < 0 || length == 0)
+        {
+            Console.WriteLine();
+        }
+
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < x; y++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("\\");
+            Console.WriteLine();
+        }
+    }
+} 
