9aff163fcfcb870ce468dd9b27e3b0519f58a883
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 16:56:11 -0400

    add task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 7; i++)  
+        {
+            for(int j = 0; j <= 9; j++)  
+            {
+                if (i < j && i != j)
+                {
+                    Console.Write("{0}{1}, ",i,j);
+                }
+            }
+        }
+        Console.WriteLine("89");
+    }
+}
