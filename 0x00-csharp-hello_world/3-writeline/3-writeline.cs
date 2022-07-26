68b8f587a17762a0f7624d300c19a82fd4914bbe
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-25 20:25:50 -0400

    change syntax

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,14 @@
-﻿// prints message
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace writeline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
