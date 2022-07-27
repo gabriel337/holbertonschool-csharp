ff36035d81cb70a1d082c952854a24261a8e5a12
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 19:34:04 -0400

    add task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        List<int> list = new List<int>();
+        int x = 0;
+
+        if (size > 0)
+        {
+            for (x = 0; x < size; x++)
+            {
+                list.Add(x);
+                Console.Write(list[x]);
+                if (x != size - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+        Console.WriteLine();
+        return list;
+    }
+} 
