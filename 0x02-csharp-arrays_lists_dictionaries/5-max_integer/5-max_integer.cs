ea430f7bceefa5aeee21b01a74e09b726656410a
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 19:48:01 -0400

    add task 5

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int max = myList.Max();
+        return (max);
+    }
+}
