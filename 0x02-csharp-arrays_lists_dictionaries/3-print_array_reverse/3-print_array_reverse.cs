24af7b2649785da0c7e9974ae6f20cc4697fce1c
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 18:58:39 -0400

    add task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+        int[] reverse = Enumerable.Reverse(array).ToArray();
+        Console.WriteLine(String.Join(',', reverse));
+        }
+    }        
+}
