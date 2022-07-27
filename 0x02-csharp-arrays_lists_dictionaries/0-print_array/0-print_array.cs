9eb33f8ccc0858c3e709124cc37e0d705153a877
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 15:35:25 -0400

    add task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            System.Environment.Exit(0);
+        }
+        int[] array = new int[size];
+        int x;
+
+        for (x = 0; x < size; x++)
+        {
+            array[x] = x;
+            Console.Write(array[x] + " ");
+        }
+        Console.WriteLine();
+        return array;
+    }
+} 
