3eff4bfb3405664edc91902befd229a19c2d5ac7
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-26 21:38:15 -0400

    add task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class FizzBuzz
+{
+    public static void Main(string[] args)
+    {
+        for (int x = 1; x <= 100; x++)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (x % 5 == 0 && x != 100)
+            {
+                Console.Write("Buzz ");
+            }
+            else
+            {
+                Console.Write("{0}", x);
+            }
+        }
+        Console.WriteLine("Buzz");
+    }
+}
