b474a70210d549dc8ca2a6b3bdaff84a506e313c
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 19:49:43 -0400

    add task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        LinkedList<int>.Enumerator num = myLList.GetEnumerator();
+        while (num.MoveNext())
+        {
+            int res = num.Current;
+            if (i == n)
+            {
+                return res;
+            }
+            i++;
+        }
+        return (0);
+    }
+}
