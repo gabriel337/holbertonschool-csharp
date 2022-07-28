c204e69b237cfe4d4f45ec278617fc66b06480d6
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 15:02:57 -0400

    add task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        var myHashSet = new HashSet<int>();
+
+        foreach (int number in myList)
+        {
+            myHashSet.Add(number);
+        }
+
+        foreach (int uniqueNumber in myHashSet)
+        {
+            sum += uniqueNumber;
+        }
+        return (sum);
+    }
+}
