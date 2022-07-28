05a6d3b2e24160a0d350c664108d25ba316eb4eb
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 19:30:38 -0400

    add task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        LinkedList<int>.Enumerator num = myLList.GetEnumerator();
+        while (num.MoveNext())
+        {
+            int res = num.Current;
+            if (res == value)
+            {
+                return i;
+            }
+            i++;
+        }
+        return (-1);
+    }
+}
