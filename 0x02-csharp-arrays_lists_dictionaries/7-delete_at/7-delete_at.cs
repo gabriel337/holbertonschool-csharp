c8f3e7ec02a1faf8b12e91b923948555805368dc
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 20:35:39 -0400

    add taks 7

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -10,19 +10,7 @@ class List
             Console.WriteLine("Index is out of range");
             return (myList);
         }
-        List<int> new_list = new List<int>();
-        for (int i = 0; i < myList.Count; i++)
-        {
-            if(myList[i] == index)
-            {
-                myList.Remove(myList[index]);
-            }
-            else
-            {
-                new_list.Add(i);
-        
-            }
-        }
-        return new_list;
+        myList.Remove(myList[index]);
+        return (myList);
     }
 }
