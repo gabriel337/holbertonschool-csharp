b73b1f626b6f6ae4bea9cd7927950ff9f09fe3f0
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 15:20:29 -0400

    add task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> x = new List<int>();
+        for (int i = 0; i < list1.Count; i++ )
+        {
+            for (int j = 0; j < list2.Count; j++)
+            {
+                if (list1[i] == list2[j])
+                {
+                    x.Add(list1[i]);
+                    }
+            }
+        }
+    return x;
+    }
+}
