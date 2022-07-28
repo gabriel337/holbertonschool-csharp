e38241c27845205b9025dbfd3c990eebc8c46fe2
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 22:28:40 -0400

    add task 14

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int[,] array = new int[5, 5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, {0, 0, 0, 0 ,0}, {0, 0 ,0 ,0 ,0} };
-        array[2, 2] = 1;
-        for (int i = 0; i < array.GetLength(0); i++)
+        int i, j;
+        int[,] arrays = new int[,] 
+        { 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 1, 0, 0}, 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 0, 0, 0}
+        };
+
+        for (i = 0; i < arrays.GetLength(0); i++)
         {
-            for (int j = 0; j < array.GetLength(1); j++) {
-                Console.Write("{0} ", array[i, j]);
-                if(j != array.GetLength(1) - 1)
+            for (j = 0; j < arrays.GetLength(1); j++)
+            {
+                Console.Write(arrays[i, j]);
+                if (j != arrays.GetLength(1) - 1)
                 {
                     Console.Write(" ");
                 }
