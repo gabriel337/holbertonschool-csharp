48bfa7e482282af17d424b80c7ebe210274028aa
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 16:15:42 -0400

    add task 2

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -7,7 +7,7 @@ class List
     {
         var differences1 = list1.Except(list2);
         var differences2 = list2.Except(list1);
-        List<int> new_list = new List<int>(); 
+        var new_list = new List<int>(); 
         foreach (var dif in differences1)
         {
             new_list.Add(dif);
