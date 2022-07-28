2848418338782ed8ed609f280202f6805db46f75
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 19:38:28 -0400

    add task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return (0);
+        }
+        LinkedList<int>.Enumerator num = myLList.GetEnumerator();
+        while (num.MoveNext())
+        {
+            int res = num.Current;
+            return res;
+            
+        }
+        return (0);
+    }
+}
