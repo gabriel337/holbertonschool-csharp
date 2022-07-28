e177bf560eb7e2e70ef1b789d304ff22e11b01e2
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 19:55:37 -0400

    add task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int total = 0;
+        LinkedList<int>.Enumerator num = myLList.GetEnumerator();
+        while (num.MoveNext())
+        {
+            int res = num.Current;
+            total += res;
+        }
+        return (total);
+    }
+}
