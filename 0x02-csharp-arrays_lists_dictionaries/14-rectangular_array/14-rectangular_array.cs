df0110b1b48b093af767f0cb61ec645d197af9d5
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 22:24:57 -0400

    add task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, {0, 0, 0, 0 ,0}, {0, 0 ,0 ,0 ,0} };
+        array[2, 2] = 1;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++) {
+                Console.Write("{0} ", array[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
