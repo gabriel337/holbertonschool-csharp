b5e98e63e935206169c6f84c930055df98c730a1
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-27 21:55:48 -0400

    add task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        foreach (KeyValuePair<string, string> author in myDict.OrderBy(key => key.Key))
+        {
+            Console.WriteLine("{0}: {1}", author.Key, author.Value);
+        }
+    }  
+}
