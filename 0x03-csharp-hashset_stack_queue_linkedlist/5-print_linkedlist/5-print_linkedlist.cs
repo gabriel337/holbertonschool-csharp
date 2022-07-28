da18187fdba176ec6fa201922ed08c53944bc4b8
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 17:29:28 -0400

    add task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> LL = new LinkedList<int>();
+        if (size < 0)
+        {
+            return LL;
+        }
+
+        for (int num = 0; num < size; num++)
+        {
+            LL.AddLast(num);
+            Console.WriteLine(num);
+        }
+        return LL;
+    }
+}
