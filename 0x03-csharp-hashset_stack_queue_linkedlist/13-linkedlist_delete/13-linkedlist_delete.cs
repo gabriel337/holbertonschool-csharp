cf5e6138fbe5e5ec23b9894050b8ade8ba2f731e
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-07-28 20:44:04 -0400

    add task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> num = myLList.First;
+        for (int i = 0; i <= index && num != null; i++)
+        {
+            if (i != index)
+            {
+                num = num.Next;
+                continue;
+            }   
+            myLList.Remove(num);
+        }
+    }
+}
