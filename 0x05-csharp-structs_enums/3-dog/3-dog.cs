cae47dce82ebd38db87119d7d4ac6175640fc15d
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-08 23:53:30 -0400

    add task 3

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+
+    public Dog (string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return "Dog Name: " + this.name + "\n" +
+                "Age: " + this.age + "\n" +
+                "Owner: " + this.owner + "\n" +
+                "Rating: " + this.rating;
+    }
+}
