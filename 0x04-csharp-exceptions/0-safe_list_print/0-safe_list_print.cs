a48cbd951b9af64628183aee48e1ff8107d9626c
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-08 21:42:57 -0400

    add task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{    
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        
+        for (int x = 0; x < n; x++)
+        {
+            try
+            {
+                Console.WriteLine(myList[x]);
+                count++;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                break;
+            }
+        }
+        return (count);
+    }
+}
