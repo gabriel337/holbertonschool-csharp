97b81413c2c83644f7aad027627895434a74d422
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-09 17:46:28 -0400

    add task 1

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -5,11 +5,17 @@ namespace Enemies
     /// <summary>Represents a zombie.</summary>
     public class Zombie
     {
+        /// <summary>
+        /// health attribute
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// new instance of Zombie
+        /// </summary>
         public Zombie()
         {
-            public int health = 0;
+            health = 0;
         }
-
-
+        
     }
 }
