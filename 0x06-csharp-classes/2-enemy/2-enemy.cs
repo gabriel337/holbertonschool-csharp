d7430e8f7d1555ba2a3f1c7e79467b855b906b18
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-09 20:01:53 -0400

    add task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// <summary>Represents a zombie.</summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// health attribute
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// new instance of Zombie
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// brings value to health
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {       
+                if (value >= 0)
+                {
+                    this.health = value;
+                }
+                else if(value < 0)
+                {
+                    throw new ArgumentException("Health must be greater than or equal to 0");
+                }             
+        }
+    }
+}
