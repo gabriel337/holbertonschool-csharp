da75322996875b9ec1549198ccac49b1b6df7a58
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-09 20:35:34 -0400

    add task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// <summary>Represents a zombie.</summary>
+    public class Zombie
+    {
+        /// health attribute
+         
+        private int health;
+        /// name attribute
+        private string name = "(No name)";
+        /// <summary>
+        /// getter and setter for name with new value
+        /// </summary>
+        /// <value></value>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        /// <summary>
+        /// new instance of Zombie
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// brings value to health
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {       
+                if (value >= 0)
+                {
+                    this.health = value;
+                }
+                else if(value < 0)
+                {
+                    throw new ArgumentException("Health must be greater than or equal to 0");
+                }             
+        }
+        /// <summary>
+        /// returns health
+        /// </summary>
+        /// <returns></returns>
+        public int GetHealth()
+        {   
+            return health;
+        }
+    }
+}
