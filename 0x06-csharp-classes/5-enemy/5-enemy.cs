c65d70d6436ab2f237f1057ee05ed9e389f057d6
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-09 20:50:35 -0400

    add task 5

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -56,7 +56,7 @@ namespace Enemies
         /// <returns></returns>
         public override string ToString()
         {
-            return $"Zombie name: {name} / Total Health: {health}";
+            return $"Zombie Name: {name} / Total Health: {health}";
         }
     }
 }
