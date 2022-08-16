5ffbcb1ea6f14c9954911883138851b7b0e3b1a5
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-15 22:33:40 -0400

    add task 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// empty class
+/// </summary>
+public class Animal 
+{
+
+}
+
+/// <summary>
+/// empty class
+/// </summary>
+public class Dog: Animal
+{
+
+}
