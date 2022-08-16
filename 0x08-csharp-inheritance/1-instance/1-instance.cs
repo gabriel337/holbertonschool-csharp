2b25f46072155fffec8bbe80af3c91238adebfdb
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-15 21:36:01 -0400

    add task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Public Class Obj and its methods
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Method that returns True if object is an instance of, or if the object
+    /// is an instance of class that inherited from, Array, otherwise return False. 
+    /// </summary>
+    /// <param name="obj"> object to be checked </param>
+    /// <returns> True if object is of instance of a class inherited from Array, otherwise False </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}
