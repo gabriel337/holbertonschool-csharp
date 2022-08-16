a4870ab77b0e49dcd4a95b32081b8d8782655ccc
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-15 21:24:54 -0400

    add task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Public class Obj
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Method that returns True if obj is of type int
+    /// </summary>
+    /// <param name="obj"> object </param>
+    /// <returns> True if obj is of type in, False otherwise </returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return (true);
+        
+        return (false);
+    }
+}
