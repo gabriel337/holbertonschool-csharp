a55ff64292d90280ee9ada458e58f293720eaee7
gabriel337 <g.david.lopezruiz@gmail.com>
2022-09-08 13:48:18 -0400

    add task 6

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// <summary>
+/// Public Class VectorMath
+/// </summary>
+public class VectorMath
+{
+    /// <summary>
+    /// Public method to add two vectors of the same dimensions
+    /// </summary>
+    /// <param name="vector1"> 2D or 3D vector </param>
+    /// <param name="vector2"> 2D or 3D vector </param>
+    /// <returns> New vector from added vectors or -1 otherwise </returns>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if ((vector1.Length > 3 || vector1.Length <= 1) || (vector2.Length > 3 || vector2.Length <= 1))
+        {
+            return (new double[1] {-1});
+        }
+        
+        if ((vector1.Length == 2 && vector2.Length == 3) || (vector1.Length == 3 && vector2.Length == 2))
+        {
+            return (new double[1] {-1});
+        }
+
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            double[] res = { 0, 0 };
+            res[0] = vector1[0] + vector2[0];
+            res[1] = vector1[1] + vector2[1];
+            return (res);
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] res = { 0, 0, 0 };
+            res[0] = vector1[0] + vector2[0];
+            res[1] = vector1[1] + vector2[1];
+            res[2] = vector1[2] + vector2[2];
+            return (res);
+        }
+
+        return (new double[] {-1});
+    }
+}
