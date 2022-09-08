b16535b8c9d2db39dc36a39c61f247ff5ab89d4b
gabriel337 <g.david.lopezruiz@gmail.com>
2022-09-08 12:23:04 -0400

    checking task

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/// <summary>
+/// Public class VectorMath
+/// </summary>
+public class VectorMath
+{
+    /// <summary>
+    /// Public static method for the Magnitude of a 2D or 3D vector
+    /// </summary>
+    /// <param name="vector"> 2D or 3D vector </param>
+    /// <returns> Rounded to the 100th double representing vector magnitude </returns>
+    public static double Magnitude(double[] vector)
+    {
+        double res = 0f;
+
+        if (vector.Length > 3 || vector.Length <= 1)
+        {
+            return (-1);
+        }
+
+        if (vector.Length == 2)
+        {
+            res = Math.Sqrt(Math.Pow(Math.Abs(vector[0]), 2) + Math.Pow(Math.Abs(vector[1]), 2));
+        }
+        else if (vector.Length == 3)
+        {
+            res = Math.Sqrt(Math.Pow(Math.Abs(vector[0]), 2) + Math.Pow(Math.Abs(vector[1]), 2) + Math.Pow(Math.Abs(vector[2]), 2));
+        }
+
+        return (Math.Round(res), 2);
+    }
+}
