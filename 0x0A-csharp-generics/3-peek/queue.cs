155d0708d8783300f78e665d109e29468147e5e0
gabriel337 <g.david.lopezruiz@gmail.com>
2022-11-08 21:15:40 -0400

    add task 3

---
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+///<summary>A generic Queue class</summary>
+public class Queue<T>
+{
+    private Node head;
+    private Node tail;
+    private int count;
+
+    ///<summary>A Node class implementation within a Queue</summary>
+    public class Node
+    {
+        ///<summary>The value inside of the node</summary>
+        public T value = default(T);
+        ///<summary>The node that follows, if none value is null</summary> 
+        public Node next = null;
+
+        ///<summary>Constructor method for a Node class</summary>
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    ///<summary>Adds a new node at the end of the queue</summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count += 1;
+        
+    }
+
+    ///<summary>Removes the first node in the queue and returns the value</summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            tail.value = head.value;
+            head = head.next;
+            count -= 1;
+            return tail.value;
+        }
+    }
+
+    ///<summary>Returns the value of the first node in the queue without removing it</summary>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            return head.value;
+        }
+    }
+
+    ///<summary>Returns the type of the Queue that was created</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    ///<summary>Returns the number of nodes in the queue</summary>
+    public int Count()
+    {
+        return count;
+    }
+}
