28907fad61e5671b53034c22b00dec87cc931b3b
gabriel337 <g.david.lopezruiz@gmail.com>
2022-11-08 20:59:39 -0400

    add task 0

---
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>A generic Queue class</summary>
+public class Queue<T>
+{
+    ///<summary>Returns created type from Queue class</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
