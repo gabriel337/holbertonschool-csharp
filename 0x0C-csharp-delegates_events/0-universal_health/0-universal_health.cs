76961e0657db0041a5f129a55227770bffdee16a
gabriel337 <g.david.lopezruiz@gmail.com>
2022-11-10 12:38:36 -0400

    add task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+///<summary>This class represents a Player object.</summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Default constructor method.</summary>
+    ///
+    ///<param name="name">The name of the player</param>
+    ///<param name="maxHp">Represents the health points of the player</param>
+    ///
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp > 0)
+            this.maxHp = maxHp;
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary>Prints the current health of the player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
